330bc3eaa87bfea66d67d211cd62c0809a45a971
FerLedesma <ferledesma352@gmail.com>
2024-02-27 13:45:25 -0300

    FUNCIONANDO -CREACION DE LAS VALIDACIONES -ELIMINAR,MODIFICAR,ETC

--- crudSpringBoot/src/main/java/principal/controladora/ControladorRest.java
+++ crudSpringBoot/src/main/java/principal/controladora/ControladorRest.java
@@ -1,18 +1,23 @@
 package principal.controladora;
 
-import principal.dao.IInviduoDao;
+import jakarta.validation.Valid;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
 import principal.domain.Individuo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import principal.servicio.IndividuoServicio;
+
+import java.util.List;
 
 @Controller //CONTROLADOR
 public class ControladorRest {
 
     //CREAMOS EL DAO
     @Autowired
-    private IInviduoDao individuoRepository;
+    private IndividuoServicio individuoServicio;
 
 
     //ESTE ES EL CONTROLADOR MVC LA CUAL NOS LLEVA AL HOLA MUNDO (ACA COMIENZA TODO)
@@ -20,8 +25,43 @@ public class ControladorRest {
     @GetMapping("/")
     public String comienzo(Model model){
 
-        Iterable<Individuo> individuos = individuoRepository.findAll();
+        List<Individuo> individuos = individuoServicio.listaIndividuos();
         model.addAttribute("individuos", individuos);
         return "indice";
     }
+
+    //AGREGAMO EL MAPEO PARA ALTA INDIVIDUO ANEXAR
+
+    //MAPEOS PARA EL GUARDADO DEL INDIVIDUO
+    @GetMapping("/anexar")
+    public String anexar(Model model){
+        Individuo individuo = new Individuo();
+        model.addAttribute("individuo", individuo);
+        return "cambiar";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(@Valid Individuo individuo, Errors error){
+        if(error.hasErrors()){
+            return "cambiar";
+        }
+        individuoServicio.salvar(individuo);
+        return "cambiar";
+    }
+
+    //modificar
+    @GetMapping("/cambiar/{id}")
+    public String cambiar(Individuo individuo, Model model){
+        individuo = individuo = individuoServicio.localizarIndividuo(individuo);
+        model.addAttribute("individuo",individuo);
+        return "cambiar";
+    }
+
+    //ELIMINAR
+    @GetMapping("/borrar/{id}")
+    public String borrar(Individuo individuo){
+        individuoServicio.borrar(individuo);
+        return "redirect:/";
+    }
+
 }
--- crudSpringBoot/src/main/java/principal/domain/Individuo.java
+++ crudSpringBoot/src/main/java/principal/domain/Individuo.java
@@ -1,6 +1,9 @@
 package principal.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -16,10 +19,20 @@ public class Individuo implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)  //PARA QUE SE AUTOGENERE EL ID
     private long id;
+
+
+    //PARA QUE NO PERMITA CAMPOS VACIOS (DPENDENCIA VALIDATION)
+
+    @NotEmpty //--> QUE NO SEA VACIO ( SE RECOMIENDA PARA CADENAS)
     private String nombre;
+    @NotEmpty
     private String apellido;
+    @NotEmpty
     private String telefono;
+    @NotEmpty
+    @Email
     private String correo;
+    @NotNull
     private int edad;
 
 
